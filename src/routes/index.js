6205af2e1a3dcb69dadbce0bafbeb188eccf3ad2
avv <akshatvijayvergiya64@gmail.com>
2025-09-08 16:24:01 +0530

    new route for user data

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const authRoutes = require('./app/authRoutes');
 const profileRoutes = require('./app/profileRoutes');
+const userRoutes = require("./app/userRoutes");
 
 router.use("/auth", authRoutes);
 router.use("/profiles", profileRoutes);
+router.use("/user", userRoutes);
 
 module.exports = router;
